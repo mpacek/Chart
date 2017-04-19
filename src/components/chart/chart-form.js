a6ff908f4953dfec123794662f18297eef810669
Michał Pacek <michal.pacek@thecogworks.com>
2017-04-19 23:02:05 +0200

    Addes a little styles, removed unused test CHART files

--- src/components/chart/chart-form.js
+++ src/components/chart/chart-form.js
@@ -7,11 +7,11 @@ export default class ChartForm extends React.Component {
 
     render() {
         return (
-            <form className="m-form js-form">
+            <form className="m-form">
                 <label for="f-email" className="m-form__label">Email address:</label><span className="m-form__current-email"></span>
                 <div className="m-form__form">
-                    <span className="m-form__info"></span>
-                    <input id="f-email" className="m-form__email" type="email" placeholder="your email..." novalidate />
+                    <span className="m-form__info is-active">Error text</span>
+                    <input id="f-email" className="m-form__email" type="email" placeholder="Email..." novalidate />
                     <button className="m-form__button" type="submit">Submit email</button>
                 </div>
             </form>
--- src/pages/info.js
+++ src/pages/info.js
@@ -6,6 +6,9 @@ export default class InfoPage extends React.Component {
             <div className="container">
                 <article className="m-article m-article--info">
                     <h1 className="m-article__title">Informations</h1>
+                    <div className="m-article__content c-rich-text">
+                        <p>On this page you will find all neccessary information about this project</p>
+                    </div>
                 </article>
             </div>
         )
