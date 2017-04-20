e3734a1a79dfd91d40b59ab1a3e7766ab3d542ef
Michał Pacek <michal.pacek@thecogworks.com>
2017-04-20 22:00:17 +0200

    Content Format fix

--- src/pages/chart.js
+++ src/pages/chart.js
@@ -2,16 +2,16 @@ import React from 'react';
 import ChartBox from '../components/chart/chart-box';
 
 export default class ChartPage extends React.Component {
-  render() {
-    return (
-        <div className="container">
-            <article className="m-article m-article--chart">
-                <h1 className="m-article__title">Weather chart</h1>
-            </article>
-            <div className="m-article__content">
-                <ChartBox apiUrl="api/chart/charts.json" />
+    render() {
+        return (
+            <div className="container">
+                <article className="m-article m-article--chart">
+                    <h1 className="m-article__title">Weather chart</h1>
+                </article>
+                <div className="m-article__content">
+                    <ChartBox apiUrl="api/chart/charts.json" />
+                </div>
             </div>
-        </div>
-    )
-  }
+        )
+    }
 }
